1743e3641bf01da8a39201a8f7864c99cf3857e0
Jonus23 <johansr265@gmail.com>
2023-11-15 22:59:37 -0500

    ingreso actualización

--- nn.py
+++ nn.py
@@ -1,6 +1,31 @@
 import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
+import mysql.connector
+
+def Inicio ():
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="jasr2004.", 
+        database="proyecto-final"
+    )
+        
+    cursor = conexion.cursor()
+    usuario = cursor.execute('SELECT usuario')
+    contraseña = cursor.execute('SELECT contraseña')
+
+    Usu = usuarioEntry.get()
+    password = contraseñaEntry.get()
+    
+    
+    if Usu == usuario and password == contraseña:
+        mensaje.config(text='Inicio de sesión valida')
+    else:
+        mensaje.config(text='Inicio de sesión invalida')
+
+    cursor.close()
+    conexion.close()
 
 root = tk.Tk()
 root.title('Inicio de sesión')
@@ -8,8 +33,8 @@ root.geometry('400x330')
 root.config(bg='#589494')
 root.resizable(0,0)
 
-mensaje_label = tk.Label(root, text="",bg='#589494')
-mensaje_label.pack()
+label = tk.Label(root, text="",bg='#589494')
+label.pack()
 
 img = Image.open('proyecto\photo.jpg')
 img.thumbnail((200, 200))
@@ -18,27 +43,26 @@ img = ImageTk.PhotoImage(img)
 imgFoto = tk.Label(image=img)
 imgFoto.pack()
 
-mensaje_label = tk.Label(root, text="",bg='#589494')
-mensaje_label.pack()
+mensaje = tk.Label(root, text="",bg='#589494')
+mensaje.pack()
 
 usuario = Label(root, text='Usuario', bg='#589494')
 usuario.pack()
 
-usuario = tk.Entry(root)
-usuario.pack()
+usuarioEntry = tk.Entry(root)
+usuarioEntry.pack()
 
-password = Label(root, text='Contraseña', bg='#589494')
-password.pack()
+contraseña_ = Label(root, text='Contraseña', bg='#589494')
+contraseña_.pack()
 
-contraseña = tk.Entry(root, show="*")  
-contraseña.pack()
+contraseñaEntry = tk.Entry(root, show="*")  
+contraseñaEntry.pack()
 
-mensaje_label = tk.Label(root, text="",bg='#589494')
-mensaje_label.pack()
+label = tk.Label(root, text="",bg='#589494')
+label.pack()
 
-botton = tk.Button(root, text='Iniciar Sesión')
-botton.pack()
-
+boton = tk.Button(root, text='Iniciar Sesión', command= Inicio)
+boton.pack()
 
 
 
